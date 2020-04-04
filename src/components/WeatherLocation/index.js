e1f4841a954488ce94e9c6caa2bb6d81d20ec872
hernan <hernanfiuba2009@gmail>
2020-04-04 17:38:51 -0300

    Fetch weather data from openWeatherMap

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,21 +1,20 @@
 import React, {Component} from 'react';
 import Location from './Location'
 import WeatherData from './WeatherData';
-import {SUNNY, CLOUDY} from '../../constants/weathers';
+import {SUNNY} from '../../constants/weathers';
 import './styles.css';
+
+const location = "Buenos Aires,ar";
+const api_key = "61395e1581dd6fffe8717fe37b652473";
+const url_location_weather = "http://api.openweathermap.org/data/2.5/weather";
+
+const url = `${url_location_weather}?q=${location}&appid=${api_key}`;
 
 const data = {
     tempeture: 25,
     weatherState: SUNNY,
     humidity: 80,
     wind: "10 m/s"
-};
-
-const data2 = {
-    tempeture: 5,
-    weatherState: CLOUDY,
-    humidity: 20,
-    wind: "30 m/s"
 };
 
 class WeatherLocation extends Component {
@@ -26,13 +25,34 @@ class WeatherLocation extends Component {
             data : data
         };
     }
+    getWeatherState = weatherData => {
+        return SUNNY;
+    }
+    getWeather = weatherData => {
+        const {temp, humidity} = weatherData.main;
+        const {speed} = weatherData.wind;
+        const weatherState = this.getWeatherState();
+        const weather = {
+            tempeture : temp,
+            weatherState,
+            humidity,
+            wind : speed,
+        };
+        return weather;
+    }
 
     updateWeatherData = () => {
         console.log("updated");
-
-        this.setState({
-            data: data2,
-        })
+        fetch(url).then(resolve => {
+            return resolve.json();
+        }).then( data => {
+            const newWeather = this.getWeather(data);
+            console.log(newWeather)
+            this.setState({
+                data: newWeather,
+            })
+            debugger;
+        });
     }
 
     render() {
